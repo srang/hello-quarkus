d865db153da6c3bfb2d88ab29f94bfdd999440af
Sam Rang <srang2010@gmail.com>
2019-10-25 12:04:39 -0400

    bootstrap

---
+++ src/test/java/com/github/srang/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.srang;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
